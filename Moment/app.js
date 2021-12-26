ae1331a9075a4741869de974f7471b66f74279e5
Seugnjun <minsj47@gmail.com>
2021-12-26 22:38:02 +0900

    Day5-(2)

---
+++ Moment/app.js
@@ -0,0 +1,20 @@
+
+const caluculater={
+    plus :function(a,b){
+        return a+b;
+    },
+    minus : function(a,b){
+        return a-b;
+    },
+};
+
+const var1=caluculater.plus(1,2);
+console.log(var1);
+const var2=caluculater.minus(1,2);
+console.log(var2);
+
+
+
+const ellos=document.getElementsByClassName("h1");
+const ellos=document.querySelectorAll(".hello h1");
+console.log(ellos);
