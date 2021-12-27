f6d63c065d0a3d69d85044d20554102324e6a398
Seugnjun <minsj47@gmail.com>
2021-12-27 20:10:49 +0900

    DAY6

--- Moment/app.js
+++ Moment/app.js
@@ -1,20 +1,25 @@
 
-const caluculater={
-    plus :function(a,b){
-        return a+b;
-    },
-    minus : function(a,b){
-        return a-b;
-    },
-};
-
-const var1=caluculater.plus(1,2);
-console.log(var1);
-const var2=caluculater.minus(1,2);
-console.log(var2);
-
-
-
-const ellos=document.getElementsByClassName("h1");
-const ellos=document.querySelectorAll(".hello h1");
-console.log(ellos);
+const loginForm=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const greeting=document.querySelector("#greeting");
+const ID="user";
+const HID="hidden"
+function handleclick(event){
+    event.preventDefault();
+    loginForm.classList.add(HID);
+   const user=loginInput.value;
+   localStorage.setItem(ID,user);
+    paintGreetings(user);
+}
+function paintGreetings(username){
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HID);
+}
+const saved=localStorage.getItem(ID);
+if (saved===null){
+    loginForm.classList.remove(HID);
+    loginForm.addEventListener("submit",handleclick);
+}
+else {
+    paintGreetings(saved);
+}
