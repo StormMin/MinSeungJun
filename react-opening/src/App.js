7b1d94a54a40b4b8bf290653fb9ad978f69bd4a4
Seugnjun <minsj47@gmail.com>
2022-01-05 18:07:56 +0900

    Day11-last

--- react-opening/src/App.js
+++ react-opening/src/App.js
@@ -3,36 +3,70 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [user, setUser] = useState("");
+  const [want, setWant] = useState("0");
+  const [thing, setthing] = useState("");
+  const [a, seta] = useState("1");
+  const [b, setb] = useState("현재값");
+  const onChange = (event) => {
+    setUser(event.target.value);
+  };
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+    if (user === "") return;
+    setWant(user);
+    setUser("");
   };
-  console.log(toDos);
+  const onch = (event) => {
+    setthing(parseInt(event.target.value));
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
+  useEffect(() => {
+    const Option = coins[thing];
+    if (Option != "undefined" && Option != null) {
+      seta(want / coins[thing].quotes.USD.price);
+      setb(coins[thing].symbol);
+    }
+  }, [coins[thing]]);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <h1>My To Dos{toDos.length}</h1>
+        <h1>
+          The Coins! {loading ? <strong>king</strong> : `(${coins.length})`}
+        </h1>
         <input
-          onChange={onChange}
-          value={toDo}
+          className={styles.sel}
+          value={user}
+          placeholder="write down"
           type="text"
-          placeholder="write"
+          onChange={onChange}
         ></input>
-        <button>ADD TO DO</button>
+        <button className={styles.sel}>입력</button>
+        <h2 className={styles.title}>현재잔고 :{want}$</h2>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select className={styles.sel} onChange={onch}>
+            <option value={"xx"}>select your coin</option>
+            {coins.map((item, index) => (
+              <option value={index} key={index}>
+                {item.name}: {item.quotes.USD.price} {item.symbol}
+              </option>
+            ))}
+          </select>
+        )}
       </form>
       <hr />
-      <ul>
-        {toDos.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
+      <p>
+        {a} {b}
+      </p>
     </div>
   );
 }
