ba320f208567bd71b2e5b6209c2c9151ea948c84
Seugnjun <minsj47@gmail.com>
2022-01-05 15:06:00 +0900

    DAY11-{3}

--- react-opening/src/App.js
+++ react-opening/src/App.js
@@ -2,25 +2,37 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("bye");
-  }
-  function hiFn() {
-    console.log("hi");
-    return byFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "HIDE" : "The Show"}</button>
+      <form onSubmit={onSubmit}>
+        <h1>My To Dos{toDos.length}</h1>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write"
+        ></input>
+        <button>ADD TO DO</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 }
