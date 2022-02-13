40ae81edc5946b1df0e6ec107ff07165f541f634
Seugnjun <minsj47@gmail.com>
2022-02-13 17:26:46 +0900

    next js 1-3

--- intro-nico/pages/about.js
+++ intro-nico/pages/about.js
@@ -1,9 +1,10 @@
 import NavBar from "../components/NavBar";
-
+import Head from "next/head";
+import Seo from "../components/Seo";
 export default function Potato() {
   return (
     <div>
-      <NavBar />
+      <Seo title="about" />
       <h1>about</h1>
     </div>
   );
