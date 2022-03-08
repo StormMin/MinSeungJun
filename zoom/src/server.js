46654da4cd6939ce98e03241958685839aeaac1f
Seugnjun <minsj47@gmail.com>
2022-03-08 17:40:32 +0900

    Websocket 사용법

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 const app = express();
 app.set("view engine", "pug");
@@ -9,4 +11,18 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res, next) => res.render("home"));
 app.get("/*", (req, res, next) => res.redirect("/"));
 const listenHandler = () => console.log(`Lisening on http://localhost:3000/`);
-app.listen(3000, listenHandler);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log(socket);
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.on("close", () => {
+    console.log("Disconnected from Browser 📕");
+  });
+  socket.send("King is Back");
+});
+server.listen(3000, listenHandler);
