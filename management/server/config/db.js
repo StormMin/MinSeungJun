b6c76b351f924c118b340897f780cff1b52ee280
Seugnjun <minsj47@gmail.com>
2022-03-27 22:12:36 +0900

    환경처리

--- management/server/config/db.js
+++ management/server/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 let db = mysql.createPool({
   host: "localhost",
-  port: "????",
+  port: process.env.port,
   user: "root",
-  password: "secret",
+  password: process.env.password,
   database: "mydatabase",
 });
 
